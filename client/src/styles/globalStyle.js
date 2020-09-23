2db5acd8acc07903f576ddb0dd9ffde46bdfe6ae
Twiggeh <twiggeh99@gmail.com>
2020-09-23 23:38:16 +0200

    added customOutline Style

--- client/src/styles/globalStyle.js
+++ client/src/styles/globalStyle.js
@@ -12,13 +12,28 @@ const fonts = {
 	fallback: 'Roboto',
 };
 
+/** @type {import('@emotion/core').Interpolation} */
 const outline = {
 	borderColor: colors.grayBorder,
-	borderStyle: ['1px', 'solid'],
+	borderWidth: '1px',
+	borderStyle: 'solid',
+};
+
+const customOutline = (top = 0, right = 0, bottom = 0, left = 0) => {
+	/** @type {import('@emotion/core').Interpolation} */
+	return {
+		borderColor: colors.grayBorder,
+		borderTopWidth: `${top}px`,
+		borderBottomWidth: `${bottom}px`,
+		borderLeftWidth: `${left}px`,
+		borderRightWidth: `${right}px`,
+		borderStyle: 'solid',
+	};
 };
 
 export const styles = {
 	outline,
+	customOutline,
 };
 
 export default {
