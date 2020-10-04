2bcc6a145982e893464b33310ac9363b0a06939f
Twiggeh <twiggeh99@gmail.com>
2020-10-04 17:40:39 +0200

    How to do a nav menu and how not to

---
+++ client/src/components/Navigation/components/newLinks.jsx
@@ -0,0 +1,171 @@
+const NewLinks = () => {
+	const [width] = useScreenSize();
+	const [open, setOpen] = useState(false);
+	return (
+		<>
+			<Nav>
+				<NavTitle>
+					<Link to='/art'>Twiggeh&apos;s Portfolio</Link>
+				</NavTitle>
+				<NavUl>
+					<NavLi>
+						<Link to='/art'>Art</Link>
+					</NavLi>
+					<NavLi>
+						<Link to='/projects'>Projects</Link>
+					</NavLi>
+					<NavLi>
+						<Link to='/about'>Contact</Link>
+					</NavLi>
+				</NavUl>
+				{width > nq[1] ? (
+					<TopRight href={'https://github.com/Twiggeh'}>
+						{globalStyle.svgs.githubLogo}
+					</TopRight>
+				) : (
+					<TopRight
+						onClick={() => {
+							setOpen(c => !c);
+						}}>
+						{globalStyle.svgs.hamburgerMenu}
+					</TopRight>
+				)}
+				<SideSlideNav open={open}>
+					<li>
+						<Link to='/art'>Art</Link>
+						<a
+							onClick={() => {
+								setOpen(c => !c);
+							}}>
+							{globalStyle.svgs.cross()}
+						</a>
+					</li>
+					<li>
+						<Link to='/projects'>Projects</Link>
+					</li>
+					<li>
+						<Link to='/about'>Contact</Link>
+					</li>
+					<li>
+						<a href='https://github.com/Twiggeh'>GitHub</a>
+					</li>
+				</SideSlideNav>
+			</Nav>
+		</>
+	);
+};
+
+export default NewLinks;
+
+var mq = globalStyle.queries.mainQueries;
+var nq = globalStyle.queries.numQueries;
+
+var Nav = styled.nav`
+	display: flex;
+	width: 100vw;
+	align-items: center;
+	justify-content: space-between;
+	background-color: ${globalStyle.colors.darkestInfill};
+	${globalStyle.styles.customOutline(0, 0, 1)};
+	a,
+	svg {
+		text-decoration: none;
+		text-transform: uppercase;
+		letter-spacing: 0.135rem;
+		&:hover {
+			color: hotpink;
+			fill: hotpink;
+			cursor: pointer;
+			line {
+				stroke: hotpink !important;
+			}
+		}
+	}
+	li {
+		display: block;
+	}
+	${[mq[1]]} {
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
+`;
+
+var NavTitle = styled.div`
+	padding-left: ${globalStyle.styles.contentPaddingSides};
+	a {
+		font-weight: 700;
+		font-size: ${globalStyle.styles.navTitleFontSize};
+	}
+`;
+
+var NavUl = styled.ul`
+	display: flex;
+	padding-top: 2em;
+	padding-bottom: 2em;
+	a {
+		font-size: ${globalStyle.styles.navElFontSize};
+	}
+	/* traditional way of hiding elements, doesn't work with screen readers
+	TODO replace with useDisplayTrans 
+	*/
+	transition: transform 0.3s 0.1s ease-in-out, width 0s 0.3s;
+	${[mq[1]]} {
+		transform: translateX(100vw);
+		width: 0;
+	}
+	@media (min-width: ${nq[1]}px) {
+		transform: translateX(0);
+		width: unset;
+	}
+`;
+
+var NavLi = styled.li`
+	:not(:first-child) {
+		margin-left: 3rem;
+	}
+`;
+
+var TopRight = styled.a`
+	margin-right: 1.5rem;
+	svg {
+		height: clamp(calc(${globalStyle.styles.navElFontSize} * 1.5), 3vw, 3vw);
+	}
+`;
+
+var SideSlideNav = styled.nav`
+	background-color: ${globalStyle.colors.darkestInfill};
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	position: absolute;
+	top: 0;
+	right: 0;
+	transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
+	transition: transform 0.5s ease;
+	padding-top: 1.5rem;
+	padding-left: 0.7rem;
+	${globalStyle.styles.customOutline(0, 0, 0, 1)}
+	a {
+		font-size: ${globalStyle.styles.navElFontSize};
+	}
+	li {
+		padding: 1rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	@media (min-width: ${nq[1]}px) {
+		display: none;
+	}
+	li > a > svg {
+		padding-left: 5rem;
+		height: ${globalStyle.styles.navElFontSize};
+	}
+`;
+
+import styled from '@emotion/styled';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import globalStyle from '../../../styles/globalStyle';
+import useScreenSize from './hooks/useScreensize';
--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,111 +1,15 @@
-const mq = globalStyle.queries.mainQueries;
-
 const Navigation = () => (
-	<nav
+	<div
 		css={{
 			fontFamily: 'Montserrat',
 			backgroundColor: globalStyle.colors.bgInfill,
 			color: globalStyle.colors.whiteText,
 			a: { color: globalStyle.colors.whiteText },
+			// TODO : re-enable
 			//overflow: 'hidden',
 		}}>
 		<BrowserRouter>
-			<ul
-				css={{
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'space-between',
-					paddingTop: '2em',
-					paddingBottom: '2em',
-					background: globalStyle.colors.darkestInfill,
-					...globalStyle.styles.customOutline(0, 0, 1),
-					a: {
-						textDecoration: 'none',
-						'&:hover': { color: 'hotpink' },
-						fontSize: globalStyle.styles.navElFontSize,
-						fontWeight: '400',
-						paddingLeft: '1em',
-						textTransform: 'uppercase',
-						letterSpacing: '0.135em',
-					},
-					[mq[1]]: {
-						position: 'fixed',
-						width: '100vw',
-						top: 0,
-						left: 0,
-						right: 0,
-						a: {
-							padding: '0',
-							paddingLeft: globalStyle.styles.contentPaddingSides,
-							paddingRight: globalStyle.styles.contentPaddingSides,
-						},
-						div: {
-							flexFlow: 'column nowrap',
-							position: 'fixed',
-							top: 0,
-							right: 0,
-							height: '100vh',
-							//paddingRight: '3rem',
-							li: {
-								paddingTop: '2rem',
-							},
-							backgroundColor: globalStyle.colors.darkestInfill,
-						},
-						paddingBottom: '1rem',
-						paddingTop: '1rem',
-					},
-				}}>
-				<li
-					css={{
-						a: {
-							fontWeight: 700,
-							fontSize: 'clamp(20px, 2vw, 32px)',
-						},
-					}}>
-					<Link to='/art'>Twiggeh&apos;s Portfolio</Link>
-				</li>
-				<div css={{ display: 'flex' }}>
-					<li css={{ display: 'flex' }}>
-						<Link to='/art'>Art</Link>
-						<a
-							css={{
-								display: 'none',
-								// padding Left spaces the entire slide menu out
-								svg: {
-									height: '1rem',
-									paddingLeft: '4rem',
-									':hover': { fill: 'hotpink' },
-									line: {},
-								},
-								[mq[1]]: {
-									display: 'inline',
-								},
-							}}>
-							{globalStyle.svgs.cross()}
-						</a>
-					</li>
-					<li>
-						<Link to='/projects'>Projects</Link>
-					</li>
-					<li>
-						<Link to='/about'>Contact</Link>
-					</li>
-				</div>
-				<li css={{ padding: 0 }}>
-					<a
-						href='https://github.com/Twiggeh'
-						css={{
-							height: '4vw',
-							width: '4vw',
-							minWidth: '40px',
-							padding: 0,
-							svg: { height: '4vw' },
-							paddingRight: '2rem',
-						}}>
-						{globalStyle.svgs.githubLogo}
-					</a>
-				</li>
-			</ul>
+			<NewLinks />
 			<Switch>
 				<Route path='/about'>
 					<AboutPage />
@@ -118,7 +22,7 @@ const Navigation = () => (
 				</Route>
 			</Switch>
 		</BrowserRouter>
-	</nav>
+	</div>
 );
 
 export default Navigation;
@@ -128,3 +32,4 @@ import globalStyle from '../../styles/globalStyle';
 import AboutPage from '../About';
 import ArtPage from '../Art';
 import ProjectsPage from '../Projects';
+import NewLinks from './components/newLinks';
