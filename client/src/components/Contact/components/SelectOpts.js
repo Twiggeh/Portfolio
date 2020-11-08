fdbb03aa835c54ef6f053e1da4d126af3e124cb8
Twiggeh <twiggeh99@gmail.com>
2020-11-08 15:59:34 +0100

    changed wording on select items

--- client/src/components/Contact/components/SelectOpts.js
+++ client/src/components/Contact/components/SelectOpts.js
@@ -1,12 +1,12 @@
 const SelectOpts = [
 	{ txt: 'Build my Idea !', value: 'software' },
-	{ txt: 'I want you to paint something !', value: 'art' },
+	{ txt: 'Paint me something !', value: 'art' },
 	{
 		txt: 'I want to know how to create Illustrations !',
 		value: 'art training',
 	},
 	{
-		txt: 'I want help with the software that I am writing !',
+		txt: 'Help me with the software I am writing !',
 		value: 'software training',
 	},
 	{ txt: 'Other ...', value: 'other' },
