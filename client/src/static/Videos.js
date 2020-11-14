1e47e4cf3968b53dc31796fd893a244735675b1b
Twiggeh <twiggeh99@gmail.com>
2020-11-14 22:00:12 +0100

    added stateless video

--- client/src/static/Videos.js
+++ client/src/static/Videos.js
@@ -1,2 +1,4 @@
 // ReplayBot UI Video
 export { default as ReplayBotUi } from '../assets/video/RepBotUI.m4v';
+// ReplayBot Stateless Video
+export { default as ReplayBotStateless } from '../assets/video/RepBotStateless.m4v';
