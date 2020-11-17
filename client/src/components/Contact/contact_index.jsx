baaa8ed36836d4b893630080316a593c04eafb5d
Twiggeh <twiggeh99@gmail.com>
2020-11-17 23:54:40 +0100

    added contact info

--- client/src/components/Contact/contact_index.jsx
+++ client/src/components/Contact/contact_index.jsx
@@ -1,31 +1,60 @@
 import styled from '@emotion/styled';
 import React from 'react';
-import { styles, svgs } from '../../styles/globalStyle';
+import { fontSizes, queries, styles, svgs } from '../../styles/globalStyle';
 import MediumButtons from '../MediumButtons';
 import Title from './components/components/Title';
 import Form from './components/Form';
 import CV from 'assets/cv website/CV_Twiggeh.pdf';
 
+const mq = queries.mainQueries;
+
 const buttons = [{ svg: svgs.Download, btnName: 'Download', btnUrl: CV }];
 
 const Contact = () => {
 	return (
-		<ConactSpacer>
+		<ContactWrap>
 			<Form />
 			<div>
+				<Title>About me</Title>
+				<TextBlock>
+					Hi I&apos;m Twig and I debranch code. Puns aside I love to create art and
+					software.
+					<br />
+					<br />I like to focus on high fidelity Characters and Splash Art. My preferred
+					language is javascript, no matter whether its in the browser or on the server.
+					<br />
+					<br />I use Node, React, MongoDB and Express.
+				</TextBlock>
 				<Title>Curriculum Vitae</Title>
-				<MediumButtons buttons={buttons} />
+				<MediumButtons
+					buttons={buttons}
+					customCss={`margin-bottom: 2.5rem;${styles.outline}`}
+				/>
 			</div>
-		</ConactSpacer>
+		</ContactWrap>
 	);
 };
 
 // TODO : Add picture of cv to href
 
-const ConactSpacer = styled.div`
+var TextBlock = styled.div`
+	font-size: ${fontSizes.text};
+	margin-bottom: 2.5rem;
+`;
+
+var ContactWrap = styled.div`
 	display: flex;
-	justify-content: space-between;
 	margin: ${styles.contentPaddingSides};
+	& > * {
+		margin-right: 10vw;
+	}
+	${[mq[0]]} {
+	}
+	${mq[1]} {
+		width: 92%;
+		margin-top: 7rem;
+		flex-wrap: wrap-reverse;
+	}
 `;
 
 export default Contact;
