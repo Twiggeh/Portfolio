d54cfa79ea4b51a574d5f61d35af428fff8cd139
Twiggeh <twiggeh99@gmail.com>
2020-11-18 22:47:10 +0100

    added certbot copy command

---
+++ server/script/copyCerts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo cp /home/twiggeh/certbot/etc/live/www.twiggeh.xyz/privkey.pem ./cert/privkey.pem
+&& sudo cp /home/twiggeh/certbot/etc/live/www.twiggeh.xyz/privkey.pem ./cert/privkey.pem
