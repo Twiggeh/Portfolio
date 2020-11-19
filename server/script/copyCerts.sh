704749156ad6f3f05e16069303b16b94dc7de33a
Twiggeh <twiggeh99@gmail.com>
2020-11-19 13:47:19 +0000

    Wildcard Domain

--- server/script/copyCerts.sh
+++ server/script/copyCerts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo cp /home/twiggeh/certbot/etc/live/www.twiggeh.xyz/privkey.pem ./cert/privkey.pem
-&& sudo cp /home/twiggeh/certbot/etc/live/www.twiggeh.xyz/privkey.pem ./cert/privkey.pem
+sudo cp /home/twiggeh/certbot/etc/live/twiggeh.xyz-0001/privkey.pem ./cert/privkey.pem
+&& sudo cp /home/twiggeh/certbot/etc/live/twiggeh.xyz-0001/fullchain.pem ./cert/fullchain.pem
