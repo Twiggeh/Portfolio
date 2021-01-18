dd80d7c9d7b74f2f11d09d9af061a8f8adcfedec
Twiggeh <twiggeh99@gmail.com>
2021-01-18 20:05:30 +0100

    OptionList is typed

---
+++ client/src/components/Contact/components/OptionList.tsx
@@ -0,0 +1,35 @@
+import Option from './Option';
+import React from 'react';
+import type { Option as IOption } from './Select';
+
+interface IOptionList {
+	options: IOption[];
+	defaultOption: IOption;
+	opened: boolean;
+}
+
+const OptionList: React.FC<IOptionList> = ({ options, defaultOption, opened }) => {
+	return (
+		<>
+			{options.map(({ txt, value, action }, i) => {
+				if (!opened && i === 0) {
+					value = defaultOption.value;
+					txt = defaultOption.txt;
+					action = defaultOption.action;
+				}
+				return (
+					<Option
+						txt={txt}
+						value={value}
+						index={i}
+						key={value}
+						listLength={options.length}
+						action={action}
+					/>
+				);
+			})}
+		</>
+	);
+};
+
+export default OptionList;
