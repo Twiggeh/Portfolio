f1e2701cc5689fc2484178ff1781aa1bbe7ed38d
Twiggeh <twiggeh99@gmail.com>
2021-01-21 16:48:34 +0100

    static file imports are typed

--- client/src/@types/global.d.ts
+++ client/src/@types/global.d.ts
@@ -1,8 +1,38 @@
 type CustomCSS = {
-	css?: string;
+	scss?: string;
 };
 
 declare module '*.svg' {
+	const content: React.FC;
+	export default content;
+}
+
+declare module '*.pdf' {
 	const content: string;
 	export default content;
+}
+
+declare module '*.webp' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.png' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.m4v' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.gif' {
+	const content: string;
+	export default content;
+}
+
+declare module 'pictures' {
+	const pictures = await import('../static/Pictures');
+	export default pictures;
 }
