5cd320030cacd56aa0b34d8dc17fd031b990e035
Twiggeh <twiggeh99@gmail.com>
2021-01-23 23:15:10 +0100

    Better Form Tests

--- client/cypress/integration/mystuff/sample.spec.ts
+++ client/cypress/integration/mystuff/sample.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 describe('Routing', () => {
 	it('Loads Home page', () => {
 		cy.visit('/');
@@ -11,17 +12,31 @@ describe('Routing', () => {
 	});
 
 	it('Fills out Form', () => {
-		cy.react('Input', { props: { name: 'email' } }).type('hello@twiggeh.com');
+		cy.react('Input', { props: { name: 'email' } })
+			.type('hello@twiggeh.com')
+			.should('have.value', 'hello@twiggeh.com');
 
-		cy.react('TextArea', { props: { name: 'message' } }).type(
-			'This an automated test message'
-		);
+		cy.react('TextArea', { props: { name: 'message' } })
+			.type('This an automated test message')
+			.should('have.value', 'This an automated test message');
 
 		cy.react('Option', { props: { txt: 'Please Select A Subject' } }).click();
+		cy.react('Option', {}).should('have.length.at.least', 5);
 
-		cy.react('Option').should('have.length.at.least', 4);
-
+		let visibleAmount = 0;
+		let notVisibleAmount = 0;
 		cy.react('Option', { props: { txt: 'Paint me something !' } }).click();
+
+		cy.react('Option', {})
+			.wait(2000)
+			.each(el => {
+				const isVisible = Number(el.css('opacity')) > 0;
+				isVisible ? visibleAmount++ : notVisibleAmount++;
+			})
+			.then(() => {
+				if (visibleAmount !== 1) throw 'Bad amount of visible elements';
+				if (notVisibleAmount !== 4) throw 'Bad amount of not visible elements';
+			});
 
 		cy.react('Button', { props: { content: 'Send' } })
 			.should('exist')
