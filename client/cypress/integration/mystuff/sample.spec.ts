a095cb5bb9bb2a18b3fb7fcf23196e44112e04e9
Twiggeh <twiggeh99@gmail.com>
2021-01-27 21:19:50 +0100

    trying to add more e2e tests (all failing)

--- client/cypress/integration/mystuff/sample.spec.ts
+++ client/cypress/integration/mystuff/sample.spec.ts
@@ -1,3 +1,5 @@
+import { Button } from '../../../src/static/Projects';
+
 // safely handles circular references
 const safeStringify = (obj: any, indent = 2) => {
 	let cache: any = [];
@@ -17,60 +19,92 @@ const safeStringify = (obj: any, indent = 2) => {
 
 /* eslint-disable cypress/no-unnecessary-waiting */
 describe('Routing', () => {
-	it('Loads Home page', () => {
+	//	it('Loads Home page', () => {
+	//		cy.visit('/');
+	//		cy.waitForReact(1001);
+	//
+	//		cy.react('FeatureImg').each(image => {
+	//			// cy.wrap(image).scrollIntoView().click();
+	//			image.trigger('click');
+	//			cy.react('ModalWrapper').should('exist');
+	//			cy.react('ModalButton').should('exist').trigger('click');
+	//		});
+	//	});
+	//
+	//	it('Loads Contact Page', () => {
+	//		cy.visit('/contact');
+	//		cy.waitForReact(1001);
+	//		cy.react('Contact').should('exist');
+	//	});
+	//
+	//	it('Fills out Form', () => {
+	//		cy.react('Input', { props: { name: 'email' } })
+	//			.type('hello@twiggeh.com')
+	//			.should('have.value', 'hello@twiggeh.com');
+	//
+	//		cy.react('TextArea', { props: { name: 'message' } })
+	//			.type('This an automated test message')
+	//			.should('have.value', 'This an automated test message');
+	//
+	//		cy.react('Option', { props: { txt: 'Please Select A Subject' } }).click();
+	//		cy.react('Option').should('have.length.at.least', 5);
+	//
+	//		let visibleAmount = 0;
+	//		let notVisibleAmount = 0;
+	//		cy.react('Option', { props: { txt: 'Paint me something !' } }).click();
+	//
+	//		cy.react('Option')
+	//			.wait(2000)
+	//			.each(el => {
+	//				const isVisible = Number(el.css('opacity')) > 0;
+	//				isVisible ? visibleAmount++ : notVisibleAmount++;
+	//			})
+	//			.then(() => {
+	//				if (visibleAmount !== 1) throw 'Bad amount of visible elements';
+	//				if (notVisibleAmount !== 4) throw 'Bad amount of not visible elements';
+	//			});
+	//
+	//		cy.react('Button', { props: { content: 'Send' } })
+	//			.should('exist')
+	//			.should('not.have.a.property', 'disabled');
+	//
+	//		cy.react('Button', { props: { content: 'Send' } }).click();
+	//	});
+
+	//	it('Loads Art page', () => {
+	//		cy.visit('/art');
+	//		cy.waitForReact(1001);
+	//	});
+
+	it('Shows Modals for Buttons', () => {
 		cy.visit('/');
 		cy.waitForReact(1001);
 
-		cy.react('FeatureImg').each(image => {
-			// cy.wrap(image).scrollIntoView().click();
-			image.trigger('click');
-			cy.react('ModalWrapper').should('exist');
-			cy.react('ModalButton').should('exist').trigger('click');
-		});
-	});
-
-	it('Loads Contact Page', () => {
-		cy.visit('/contact');
-		cy.waitForReact(1001);
-		cy.react('Contact').should('exist');
-	});
-
-	it('Fills out Form', () => {
-		cy.react('Input', { props: { name: 'email' } })
-			.type('hello@twiggeh.com')
-			.should('have.value', 'hello@twiggeh.com');
-
-		cy.react('TextArea', { props: { name: 'message' } })
-			.type('This an automated test message')
-			.should('have.value', 'This an automated test message');
-
-		cy.react('Option', { props: { txt: 'Please Select A Subject' } }).click();
-		cy.react('Option').should('have.length.at.least', 5);
-
-		let visibleAmount = 0;
-		let notVisibleAmount = 0;
-		cy.react('Option', { props: { txt: 'Paint me something !' } }).click();
-
-		cy.react('Option')
-			.wait(2000)
-			.each(el => {
-				const isVisible = Number(el.css('opacity')) > 0;
-				isVisible ? visibleAmount++ : notVisibleAmount++;
-			})
-			.then(() => {
-				if (visibleAmount !== 1) throw 'Bad amount of visible elements';
-				if (notVisibleAmount !== 4) throw 'Bad amount of not visible elements';
-			});
-
-		cy.react('Button', { props: { content: 'Send' } })
+		cy.getReact('MediumButtons')
 			.should('exist')
-			.should('not.have.a.property', 'disabled');
+			.each(node => {
+				cy.wrap(node)
+					.getProps('buttons')
+					.then(buttonsProp => {
+						for (
+							let buttonIndex = 0;
+							buttonIndex < buttonsProp.length;
+							buttonIndex += 1
+						) {
+							const { btnName, modal, btnUrl }: Button = buttonsProp[buttonIndex];
 
-		cy.react('Button', { props: { content: 'Send' } }).click();
-	});
+							if (!modal) continue;
 
-	it('Loads Art page', () => {
-		cy.visit('/art');
-		cy.waitForReact(1001);
+							cy.react('BtnListWrap', { props: { href: btnUrl } })
+								.should('exist')
+								.eq(0)
+								.click()
+								.then(() => {
+									//cy.react('ModalWrapper').should('exist');
+									cy.getReact('ModalButton').nthNode(0).should('exist').trigger('click');
+								});
+						}
+					});
+			});
 	});
 });
