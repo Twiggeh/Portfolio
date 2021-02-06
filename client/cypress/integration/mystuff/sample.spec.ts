8787c58f65d6a0c048c55a7003bbd1dee34dba79
Twiggeh <twiggeh99@gmail.com>
2021-02-06 23:50:35 +0100

    Contact Page functionality

--- client/cypress/integration/mystuff/sample.spec.ts
+++ client/cypress/integration/mystuff/sample.spec.ts
@@ -82,6 +82,40 @@ describe('Content Page functionality', () => {
 		});
 	});
 });
+
+describe('Contact page functionality', () => {
+	before(() => cy.visit('/contact').waitForReact(1000));
+	it('Form works correctly', () => {
+		cy.react('Input', { props: { name: 'email' } })
+			.type('hello@twiggeh.com')
+			.should('have.value', 'hello@twiggeh.com');
+
+		cy.react('TextArea', { props: { name: 'message' } })
+			.type('This an automated test message')
+			.should('have.value', 'This an automated test message');
+
+		cy.react('Option', { props: { txt: 'Please Select A Subject' } }).click();
+		cy.react('Option').should('have.length.at.least', 5);
+
+		let visibleAmount = 0;
+		let notVisibleAmount = 0;
+		cy.react('Option', { props: { txt: 'Paint me something !' } }).click();
+
+		cy.react('Option')
+			.wait(2000)
+			.each(el => {
+				const isVisible = Number(el.css('opacity')) > 0;
+				isVisible ? visibleAmount++ : notVisibleAmount++;
+			})
+			.then(() => {
+				if (visibleAmount !== 1) throw 'Bad amount of visible elements';
+				if (notVisibleAmount !== 4) throw 'Bad amount of not visible elements';
 			});
+
+		cy.react('Button', { props: { content: 'Send' } })
+			.should('exist')
+			.should('not.have.a.property', 'disabled');
+
+		cy.react('Button', { props: { content: 'Send' } }).click();
 	});
 });
